73f038c362f4dc5bb21d1b4876cc840003827dd3
GuiDev45 <bortolettodev@gmail.com>
2022-07-12 16:08:43 -0300

    Adicionando Tema Bootstrap

---
+++ BulkyBookWeb/Controllers/CategoriaController.cs
@@ -0,0 +1,22 @@
+﻿using BulkyBookWeb.Db;
+using BulkyBookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Controllers
+{
+    public class CategoriaController : Controller
+    {
+        private readonly Contexto _db;
+
+        public CategoriaController(Contexto db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Categoria> objCategoriaList = _db.Categorias;//Recupera todas as categorias da tabela
+            return View(objCategoriaList);
+        }
+    }
+}
