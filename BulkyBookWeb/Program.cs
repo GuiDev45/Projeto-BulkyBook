f1280e2ff759ef42a467b048e4e9c308e0aea56f
GuiDev45 <bortolettodev@gmail.com>
2022-07-12 16:19:27 -0300

    Adicionando novo menu de navegação

--- BulkyBookWeb/Program.cs
+++ BulkyBookWeb/Program.cs
@@ -9,7 +9,7 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<Contexto>(options => options.UseSqlServer(
     builder.Configuration.GetConnectionString("DefaultConnection")
     ));
-
+builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
